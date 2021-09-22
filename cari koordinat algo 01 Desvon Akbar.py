14eb2c1a0c1fef5771473b605a3be0c90e8094d3
Divon2 <91172013+Divon2@users.noreply.github.com>
2021-09-22 13:21:50 +0700

    Add files via upload

---
+++ cari koordinat algo 01 Desvon Akbar.py
@@ -0,0 +1,9 @@
+import math 
+print ("cari koordinat") 
+x1 =eval(input("masukkan nilai x1=")) 
+x2 =eval(input("masukkan nilai x2="))
+y1 =eval(input("masukkan nilai y1="))
+y2 =eval(input("masukkan nilai y2=")) 
+
+print("hasilnya adalah:",math.sqrt((x1-x2)**2+(y1-y2))) 
+
